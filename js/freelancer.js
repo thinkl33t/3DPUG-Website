bf327f78f229376e7479b0cf2e89198c4dd3b250
Bob Clough <bob@clough.me>
2015-04-20 16:57:05 +0100

    Made smooth scroll disable waypoints while moving to prevent URL changing

--- js/freelancer.js
+++ js/freelancer.js
@@ -6,27 +6,38 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+    wp_inst = $('header,section').waypoint({
+      handler: function(direction) {
+        var the_id = this.element.id;
+        if (direction=='up')
+          the_id = $(this.element).prev()[0].id;
+
+        $('.page-scroll').removeClass('active');
+        var $anchor = $('a[href="#' + the_id + '"]')
+        $anchor.parent().addClass('active');
+        window.history.replaceState({}, '', $anchor.attr('href'));
+      }
+    });
+
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
         window.history.replaceState({}, '', $anchor.attr('href'));
+        wp_inst.forEach(function(waypoint) {
+          waypoint.disable()
+        });
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+              scrollTop: $($anchor.attr('href')).offset().top
+            },
+            1500,
+            'easeInOutExpo',
+            function() {
+              wp_inst.forEach(function(waypoint) {
+                waypoint.enable()
+              })
+            }
+            );
         event.preventDefault();
     });
-
-    $('header,section').waypoint({
-        handler: function(direction) {
-          var the_id = this.element.id;
-          if (direction=='up')
-            the_id = $(this.element).prev()[0].id;
-
-          $('.page-scroll').removeClass('active');
-          var $anchor = $('a[href="#' + the_id + '"]')
-          $anchor.parent().addClass('active');
-          window.history.replaceState({}, '', $anchor.attr('href'));
-        }
-      });
 });
 
 // Floating label headings for the contact form
